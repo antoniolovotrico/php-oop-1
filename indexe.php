716db3a43ea507ac8794fc502771d117c8af1b47
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-15 16:29:51 +0100

    exercise completed

---
+++ indexe.php
@@ -0,0 +1,57 @@
+<?php 
+
+/** CdCollection class
+ * @author antonio  antonio@gmail.com
+ * @copyright 2021 antonio
+ */
+
+class CdCollection {
+    public $artist;
+    public $genre;
+    public $title;
+    public $year;
+
+   function __construct(string $artist,string $genre,string $title,int $year)
+   {
+       $this -> artist = $artist;
+       $this -> genre = $genre;
+       $this -> title = $title;
+       $this -> year = $year;
+   }
+}
+
+$nevermind = new CdCollection('Nirvana','Grunge','Nevermind',1991);
+$ten = new CdCollection('Pearl Jam','Grunge','Ten',1991);
+$punkInDrublic = new CdCollection('NOFX','Punk','Punk in Drublic',1994);
+$keeper = new CdCollection('Helloween','Power Metal','Keeper of the Seven Keys',1987);
+
+$cdCollection = [$nevermind,$ten,$punkInDrublic,$keeper];
+var_dump($cdCollection);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-oop-1</title>
+    </head>
+    <body>
+       <ol>
+           <?php foreach ($cdCollection as $cd) { ?>
+               <li><h2> <?php echo $cd -> title ?> </h2></li>
+               
+                   <ul>
+                       <li><h2> <?php echo $cd -> artist ?> </h2></li>
+                       <li> <h2> <?php echo $cd -> genre  ?> </h2></li>
+                       <li> <h2> <?php echo $cd -> year ?> </h2></li>
+                    </ul>
+                
+               
+            <?php } ?>
+           </ol>
+        
+    </body>
+</html>
